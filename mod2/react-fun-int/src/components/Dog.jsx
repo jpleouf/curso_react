0c0510897bc2527c101a559a4ec6d44a4f4d46b2
JPleouf <jpleouf@hotmail.com>
2022-12-07 15:44:07 -0300

    Traductor_Calculador

--- mod2/react-fun-int/src/components/Dog.jsx
+++ mod2/react-fun-int/src/components/Dog.jsx
@@ -1,23 +1,33 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
+
 const Dog = () => {
     const [titulo,setTitulo]=useState('Dog Component');
-    
+    const [descripcion,setDescripcion]=useState('');
+    const [data, setData]=useState({});
+
+    const onClickButton = ()=>{
+        setTitulo((Math.random() + 1).toString(36).substring(7));
+        setDescripcion((Math.random() + 1).toString(36).substring(7));
+    }
+        
     useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        /*fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => response.json())
-        .then(json => console.log(json));      
+        .then(json => console.log(json));*/
 
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        fetch('https://dog.ceo/api/breeds/image/random')
         .then(response => { return response.json()})
-        .then((data) => console.log(data));
+        .then((data) => {console.log(data);setData(data);});
 
-    },[titulo]);
+    },[titulo, descripcion]);
 
     return (
     <div>
-        <h1>{titulo}</h1>
+        <h1 alt={descripcion}>{titulo}</h1>
+        <button onClick={onClickButton}>Cambiar el titulo</button>
+        <img src={data.message}></img>
     </div>
     );
 }
