db356f71c95df67457523292fd4c7adb7c106caf
JPleouf <jpleouf@hotmail.com>
2022-12-13 16:09:11 -0300

    Traductor_Calculador

---
+++ mod3/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <h2>Proyecto Mod3</h2>
+    </div>
+  );
+}
+
+export default App;
