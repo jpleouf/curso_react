830b0d514d3f752d7e83f616fdc09f95c6c44859
JPleouf <jpleouf@hotmail.com>
2022-12-14 15:06:15 -0300

    MiApp_Mod3

---
+++ mod3/src/redux/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+import rootReducer from './reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export let store = configureStore(persistedReducer);
+export let persistor = persistStore(store);
+
+/*export default () => {
+  let store = configureStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor }
+}*/
