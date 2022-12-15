ff0f117c878d5e6609009f0b957ed085e6f84303
JPleouf <jpleouf@hotmail.com>
2022-12-15 15:03:12 -0300

    MiApp_Mod4

---
+++ mod4/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//const cookieParser = require('cookie-parser');
+//const compression = require('compression');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+const app = express ();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3001);
+
+app.get('*', (req, res) => {
+    return res.status(200).send({message: 'API V1 OK !'});
+})
+
+
---
+++ mod4/script.js
@@ -0,0 +1,1 @@
+console.log('Iniciando mi Server.');
